006884b28d1179a6adfc6740b9cec1ff57d8a32b
Jaime Tanori <jaime.tanori@gmail.com>
2013-03-13 00:50:57 -0700

    set /vagrant as dir to load config file from

--- attributes/default.rb
+++ attributes/default.rb
@@ -26,7 +26,7 @@
 # default['nginx']['source']['checksum']
 default['nginx']['version'] = "1.2.6"
 default['nginx']['package_name'] = "nginx"
-default['nginx']['dir'] = "/etc/nginx"
+default['nginx']['dir'] = "/vagrant"
 default['nginx']['log_dir'] = "/var/log/nginx"
 default['nginx']['binary'] = "/usr/sbin/nginx"
 
